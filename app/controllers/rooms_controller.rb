522c79507c31b36ea0d347612c892f5af53cf3bf
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-09-12 19:10:18 +0100

    Add show and private method: set_room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,16 @@
 class RoomsController < ApplicationController
+  before_action :set_room, only: [:show]
   def index
     @rooms = Room.all
   end
 
-  
+  def show
+
+  end
+
+  private
+
+  def set_room
+    @room = Room.find(params[:id])
+  end
 end
