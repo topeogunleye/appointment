f9daa9e2891ee84ab906e65a1d57eaabaf474b59
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-09-13 10:21:23 +0100

    Add Create Method

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -11,9 +11,24 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
+  def create
+    @room = Room.new(room_params)
+    respond_to do |format|
+      if @room.save
+        format.html { redirect_to room_url(@room), notice: 'Room was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def set_room
     @room = Room.find(params[:id])
+  end
+
+  def room_params
+    params.require(:room).permit(:name, :capacity)
   end
 end
